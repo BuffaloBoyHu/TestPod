236caa2f76d820df83633eba87681d4ddc9a5767
hulianghai <hulianghai@xmzhen.com>
2017-05-15 15:36:48 +0800

    fix

--- O2View/O2View.swift
+++ O2View/O2View.swift
@@ -12,10 +12,11 @@ class O2View: UIView {
 
     public override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = UIColor.redColor()
+        backgroundColor = UIColor.blue
     }
     
-    required public init?(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
 }
